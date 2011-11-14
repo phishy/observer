8b343714377eba409f98111e117ccd7dfb5c0507
Jeff Loiselle <jeff.loiselle@gmail.com>
2011-11-14 16:36:46 +0000

    changes

---
+++ app/vendors/shells/email.php
@@ -0,0 +1,80 @@
+<?php
+class EmailShell extends Shell 
+{
+	var $uses = array('Email', 'User');
+
+	function _welcome() {}
+	function _clear() {}
+
+	/**
+	 * receives an email
+	 */
+	function receive() {
+		$data = file_get_contents('php://stdin');
+		preg_match('/observer\+[A-Za-z0-9]+@automata.me/', $data, $matches);
+		if (!$matches) {
+			exit(67);
+		}
+		$email = $matches[0];
+		if ($matches) {
+			list($user, $domain) = explode('@', $email);
+			list($mailbox, $key) = explode('+', $user);
+		}
+		if (!$user = $this->User->findByKey($key)) {
+			exit(67);
+		}
+		
+		$info = $this->_parse($data);
+
+		$email = array(
+			'user_id' => $user['User']['id'],
+			'body'    => $data,
+			'from'    => $info['from'],
+			'subject' => $info['subject']
+		);
+		if ($this->Email->save($email)) {
+			exit(0);
+		} else {
+			exit(67);
+		}
+	}
+
+	/**
+	 * updates email metadata
+	 */
+	// function update_metadata() 
+	// {
+	// 	$emails = $emails = $this->Email->find('all', array('conditions' => array('from' => NULL)));
+	// 	foreach ($emails as $e) {
+	// 		$info = $this->_parse($e['Email']['body']);
+	// 		$e['Email']['from'] = $info['from'];
+	// 		$e['Email']['subject'] = $info['subject'];
+	// 		$this->Email->save($e);
+	// 	}
+	// }
+
+	/**
+	 * a dumb email parsing function
+	 */
+	function _parse($data = '') {
+		if (!$data) return;
+		$lines = explode("\n", $data);
+		if (!$lines) return;
+		foreach ($lines as $l) {
+			if (strstr($l, 'From: ')) {
+				preg_match('/[A-Za-z0-9.\-_+]+@[A-Za-z0-9]+.[A-Z-a-z]+/', $l, $matches);
+				$from = $matches[0];
+			}
+		}
+		foreach ($lines as $l) {
+			if (strstr($l, 'Subject:')) {
+				list(, $subject) = explode('Subject:', $l);
+			}
+		}
+		return array(
+			'from'    => trim($from),
+			'subject' => trim($subject)
+		);
+	}
+}
+?>
---
+++ receive-email.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+/var/www/dev.automata.me/cake/console/cake -app /var/www/dev.automata.me/app email receive
